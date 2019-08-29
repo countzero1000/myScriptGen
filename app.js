e0693cc3e9f340bf6373830e416a8714caead1f6
Edward Garmon <garmonedward@gmail.com>
2019-08-28 19:43:08 -0500

    wavs downloaded from mongodb

--- app.js
+++ app.js
@@ -1,10 +1,69 @@
-let nchars = 2 ;
-let nlines = 10;
-let pskipchar = 0.25;
+const Grid = require('gridfs-stream');
+const mongoose = require('mongoose');
+const fs = require('fs').promises;
+const mm = require('music-metadata');
+const Ciseaux = require('ciseaux');
+
+const ofs = require('fs');
+
+const mongURI = 'mongodb+srv://Admin:iamadmin@mismatch-lla7j.azure.mongodb.net/test?retryWrites=true&w=majority';
+let folderName = 'tmpScript/'
 let pLT = .2;
+let nchars = 2;
+let nlines = 14
+let pskipchar = 0.25;
+
+
+let lines = [
+{ name: 'George_Costanza', fileName: '1_George_Costanza.wav' },
+{ name: 'Jerry_Seinfeld', fileName: '2_Jerry_Seinfeld.wav' },
+{ name: 'George_Costanza', fileName: '3_George_Costanza.wav' },
+{ name: 'Jerry_Seinfeld', fileName: '4_Jerry_Seinfeld.wav' },
+{ name: 'George_Costanza', fileName: '5_George_Costanza.wav' },
+{ name: 'Jerry_Seinfeld', fileName: '6_Jerry_Seinfeld.wav' },
+{ name: 'George_Costanza', fileName: '7_George_Costanza.wav' },
+{ name: 'Jerry_Seinfeld', fileName: '8_Jerry_Seinfeld.wav' },
+{ name: 'George_Costanza', fileName: '9_George_Costanza.wav' },
+{ name: 'Jerry_Seinfeld', fileName: '10_Jerry_Seinfeld.wav' },
+{ name: 'George_Costanza', fileName: '11_George_Costanza.wav' },  { name: 'Jerry_Seinfeld', fileName: '12_Jerry_Seinfeld.wav' },
+{ name: 'George_Costanza', fileName: '13_George_Costanza.wav' },  { name: 'Jerry_Seinfeld', fileName: '14_Jerry_Seinfeld.wav' },
+{ name: 'George_Costanza', fileName: '15_George_Costanza.wav' },  { name: 'Jerry_Seinfeld', fileName: '16_Jerry_Seinfeld.wav' },
+{ name: 'George_Costanza', fileName: '17_George_Costanza.wav' },  { name: 'Jerry_Seinfeld', fileName: '18_Jerry_Seinfeld.wav' },
+{ name: 'George_Costanza', fileName: '19_George_Costanza.wav' },  { name: 'Jerry_Seinfeld', fileName: '20_Jerry_Seinfeld.wav' },
+{ name: 'George_Costanza', fileName: '21_George_Costanza.wav' },  { name: 'Jerry_Seinfeld', fileName: '22_Jerry_Seinfeld.wav' },
+{ name: 'George_Costanza', fileName: '23_George_Costanza.wav' },  { name: 'Jerry_Seinfeld', fileName: '24_Jerry_Seinfeld.wav' },
+{ name: 'George_Costanza', fileName: '25_George_Costanza.wav' },  { name: 'Jerry_Seinfeld', fileName: '26_Jerry_Seinfeld.wav' },
+{ name: 'George_Costanza', fileName: '27_George_Costanza.wav' },  { name: 'Jerry_Seinfeld', fileName: '28_Jerry_Seinfeld.wav' } ];
 
 
 
+downloadFiles = async (lines) => {
+
+    let mongoOptions = { 
+        reconnectTries: Number.MAX_VALUE, 
+        reconnectInterval: 500,
+        useNewUrlParser: true,
+        useMongoClient: true 
+      }
+
+      mongoose.connect(mongURI,mongoOptions);
+
+      mongoose.connection.once("open" , ()=>{
+
+
+       let gfs = Grid(mongoose.connection.db,mongoose.mongo);
+      
+        lines.forEach((filename) =>{
+
+        let readStream  = gfs.createReadStream({
+            filename : filename
+        });
+
+        readStream.pipe(ofs.createWriteStream(folderName+filename));
+        })
+
+    })
+}
 
 
 makeRandomArray = (min,max,length) => {
@@ -20,34 +79,33 @@ makeRandomArray = (min,max,length) => {
 
 }
 
-getLinesMetadata = (nchars,nlines,pskipchar) => {
+sortLines = async (lines,nchars,nlines,pskipchar) => {
 
-    let linesTime = [];
+    let sorted = [[],[]];
+    
 
-    for(let i = 0; i < nchars; i++){
+    for(let i = 0; i < lines.length; i++){
 
-        let linestemp = makeRandomArray(1,6,nlines);
+        if(lines[i].name == 'George_Costanza') {
 
-        linestemp.map((num) => {
+            sorted[0].push(folderName + lines[i].fileName);
 
-            if(pskipchar > Math.random()){
-                return 0;
-                } else return num;
-            })
+        }else 
 
-        
+        if(lines[i].name == 'Jerry_Seinfeld'){
 
-        linesTime.push(linestemp);
+            
+            sorted[1].push(folderName + lines[i].fileName)
+
+        }
     }
 
-    console.log(JSON.stringify(linesTime));
-
-    return linesTime;
+    return sorted;
 }
 
 
 
-buildTimeLine = (nlines,nchars,linesTime,LTtime,pLT) => {
+buildTimeLine = async (nlines,nchars,linesTime,LTtime) => {
 
     let totalLines = nchars * nlines;
 
@@ -61,53 +119,40 @@ buildTimeLine = (nlines,nchars,linesTime,LTtime,pLT) => {
     for(let p = 1; p < totalLines; p++){
 
         let currentCharacter = ((p-1) % nchars) + 1;
-        
+     
+        if(Math.random() > pskipchar){
+            
+            
 
-        let eventEndTime = maxT + linesTime[currentCharacter-1][kchar]
-
-        let event = {
-            type : 'characterLine',
-            character : currentCharacter,
-            startTime : maxT,
-            endTime : eventEndTime,
-            lineNum : kchar
+            timeLine.push( await Ciseaux.from(linesTime[currentCharacter-1][kchar]));
+            console.log(linesTime[currentCharacter-1][kchar]);
+                
         }
 
 
-        //console.log(linesTime[currentCharacter-1,kchar]);
         
-        timeLine.push(event);
-
-        maxT = eventEndTime;
         
-
 
         if(currentCharacter == nchars) kchar ++;
 
-        if(Math.random()<pLT){
 
-            let CLdelay = Cldist() + maxT; // write CLdelay
+        if(Math.random() < pLT){
 
-            let event = {
-                type : 'laughTrack',
+        
+            timeLine.push(await Ciseaux.silence(Cldist())); // write CLdelay
+            
+            timeLine.push(await Ciseaux.from('laugh track 1_01.wav'));
 
-                startTime : CLdelay,
-                endTime : CLdelay + LTtime[Math.floor((Math.random())*(LTtime.length-1))], // will change later
-                lineNum : kLT
-            }
+            timeLine.push(await Ciseaux.silence(LCdist())); // do later
 
-            console.log(LTtime.length)
-
-            maxT = event.endTime + LCdist(); // do later
-
-            timeLine.push(event)
+           
 
             kLT = kLT +1;
 
 
         } else {
 
-            maxT = maxT + CCdist();
+            timeLine.push(await Ciseaux.silence(CCdist()));
 
         }
 
@@ -115,11 +160,18 @@ buildTimeLine = (nlines,nchars,linesTime,LTtime,pLT) => {
 
     }
 
-    return timeLine;
+
+    console.log(timeLine.length);
+
+    let finalTape = await Ciseaux.concat(timeLine);
+
+    fs.writeFile('finalCut.wav', new Uint8Array(await finalTape.render()));
 
 
 
 }
+
+
 
 
 CCdist = () =>{
@@ -138,25 +190,18 @@ LCdist =() =>{
 
 }
 
+//downloadFiles(lines);
+
+main = async () => {
+
+    let sorted = await sortLines(lines);
 
 
+    buildTimeLine(nlines,nchars,sorted,pLT);
 
+}
 
-
-linesTime = getLinesMetadata(nchars,nlines,pskipchar);
-
-
-let LTtime = makeRandomArray(1,3,30);
-
-let timeline = buildTimeLine(nlines,nchars,linesTime,LTtime,pLT);
-
-console.log(timeline.length)
-
-timeline.forEach((obj)=>{
-
-    //console.log("|",obj.startTime," ",obj.endTime,"|"," ",obj.type,"|");
-    console.log(obj);
-})
+main();
 
 
 
